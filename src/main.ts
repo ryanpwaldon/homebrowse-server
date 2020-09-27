9d2cf5359c22da738de5392f0eab0f112fdba0af
Ryan Waldon <ryanpwaldon@gmail.com>
2020-09-27 19:29:12 +1000

    updates

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import * as cors from 'cors'
 import { ConfigService } from './config/config.service'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.use(cors())
+  app.enableCors()
   app.useGlobalPipes(new ValidationPipe({ disableErrorMessages: true }))
   await app.listen(process.env.PORT || app.get(ConfigService).get('PORT'))
 }
